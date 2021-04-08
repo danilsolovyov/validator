6866af23275b0e3abae1aa7cb492072b7b55b38c
Danil Solovyov <solovyevdanil@yandex.ru>
2021-04-08 16:19:59 +0500

    Fix decode.go

--- decode.go
+++ decode.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 )
 
-type decoder struct {
+type Decoder struct {
 	json.Decoder
 }
 
-func (dec *decoder) DecodeAndValidate(s interface{}, v Validator) error {
+func (dec *Decoder) DecodeAndValidate(s interface{}, v Validator) error {
 	err := dec.Decoder.Decode(s)
 	if err != nil {
 		return err
